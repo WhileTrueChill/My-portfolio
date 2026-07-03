63492f629d5667b301aa6706da3bfa36d58ae7d5
Aditya Raj <ag3854797@gmail.com>
2026-07-03 20:21:22 +0530

    Add files via upload

---
+++ protfolio/portfolio.js
@@ -0,0 +1,17 @@
+
+let typed = new Typed("#element", {
+    strings: [
+        "Frontend Developer",
+        "Full Stack Developer",
+        "AI Enthusiast",
+        "Web Designer"
+    ],
+
+    typeSpeed: 100,      // Typing speed
+    backSpeed: 60,      // Delete speed
+    backDelay: 1500,    // 1 sec rukega
+    startDelay: 500,    // Start hone se pehle delay
+    loop: true,         // Infinite loop
+    showCursor: true,   // Cursor show karega
+    cursorChar: ""     // Cursor
+});
